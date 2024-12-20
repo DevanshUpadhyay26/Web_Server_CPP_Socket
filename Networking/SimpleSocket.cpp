741c9901a983f3dab1cacafe32a2ffa1003caa00
DevanshUpadhyay26 <devanshupadhyay26@gmail.com>
2024-12-20 17:35:14 +0530

    Implemented: 1. Binding Socket 2. Connecting Socket

--- Networking/SimpleSocket.cpp
+++ Networking/SimpleSocket.cpp
@@ -1,6 +1,10 @@
 #include "SimpleSocket.hpp"
 #include <cstdlib>
 #include <stdlib.h>
+// #include <glut.h>
+
+// #include <process.h>
+// #include <stdlib.h>
 HDE::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface)
 {
 
@@ -9,14 +13,14 @@ HDE::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port,
     address.sin_addr.s_addr = htonl(interface);
     sock = socket(domain, service, protocol);
     test_connection(sock);
-    connection = connect_to_network(sock, address);
-    test_connection(connection);
+    // connection = connect_to_network(sock, address);
+    // test_connection(connection);
 }
 
 void HDE::SimpleSocket::test_connection(int item_to_test){
     if(item_to_test < 0){
         perror("Failed to connect....../");
-        exit(EXIT_FAILURE);
+        std::exit(EXIT_FAILURE); 
 
     }
 }
@@ -24,3 +28,4 @@ void HDE::SimpleSocket::test_connection(int item_to_test){
 struct sockaddr_in HDE::SimpleSocket::get_address(){return address;}
 int HDE::SimpleSocket::get_sock(){return sock;}
 int HDE::SimpleSocket::get_connection(){return connection;}
+void HDE::SimpleSocket::set_connection(int con){connection = con;}
